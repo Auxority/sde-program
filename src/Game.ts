40ac773b58f238cc9182133b62209c081330e98a
Auxority <gide0013@hz.nl>
2022-01-13 15:00:25 +0100

    resize changes

--- src/Game.ts
+++ src/Game.ts
@@ -9,6 +9,29 @@ export default class Game {
         this._canvas = document.getElementById(canvasId) as HTMLCanvasElement;
         this._ctx = this._canvas.getContext("2d") as CanvasRenderingContext2D;
         this._resizer = new Resizer(this._canvas);
+        this.createStartButton();
     }
 
+    private start(): void {
+        this._resizer.enableAutoResize();
+    }
+
+    private createStartButton(): void {
+        const button = document.createElement("button");
+        button.innerText = "Start";
+        button.id = "start-button";
+
+        document.body.appendChild(button);
+
+        this.addStartButtonEvent(button);
+    }
+
+    private addStartButtonEvent(startButton: HTMLButtonElement) {
+        startButton.addEventListener("click", () => this.onStartButtonClicked(startButton));
+    }
+
+    private onStartButtonClicked(startButton: HTMLButtonElement): void {
+        this.start();
+        startButton.remove();
+    }
 }
--- src/Resizer.ts
+++ src/Resizer.ts
@@ -1,32 +1,31 @@
 export default class Resizer {
+    private _autoResize: boolean;
     private _canvas: HTMLCanvasElement;
-    private _running: boolean = false;
 
     public constructor(canvas: HTMLCanvasElement) {
         this._canvas = canvas;
+        this._autoResize = false;
         window.addEventListener('resize', () => this.onResize());
     }
 
-    private onResize(): void {
-        if (this._running) {
-            this.resizer();
-        }
-    }
-
-    private resizer(): void {
-        this._canvas.width = window.innerWidth;
-        this._canvas.height = window.innerHeight;
+    public enableAutoResize(): void {
+        this._autoResize = true;
+        this.onResize();
     }
 
     public enableFullscreen(): void {
         this._canvas.requestFullscreen();
     }
 
-    public startGame(): void {
-        this._running = true;
-    }
-
-    public get running(): boolean {
-        return this._running;
+    /**
+     * Makes sure that the window always has a 1:1 ratio.
+     * And adjusts itself when the user resizes their browser.
+     */
+    private onResize(): void {
+        if (this._autoResize) {
+            const smallestSize: number = Math.min(window.innerWidth, window.innerHeight);
+            this._canvas.width = smallestSize;
+            this._canvas.height = smallestSize;
+        }
     }
 }
