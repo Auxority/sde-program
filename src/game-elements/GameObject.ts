214698ed775e5b1799a18cc83238f9e3edb954d4
Auxority <gide0013@hz.nl>
2022-01-14 10:47:39 +0100

    add acceleration to game object and add proper release workflow

--- src/game-elements/GameObject.ts
+++ src/game-elements/GameObject.ts
@@ -4,18 +4,22 @@ export default abstract class GameObject {
     private _ctx: CanvasRenderingContext2D;
     private _position: Vector;
     private _velocity: Vector;
+    private _acceleration: Vector;
 
     public constructor(ctx: CanvasRenderingContext2D, position: Vector) {
         this._ctx = ctx;
         this._position = position;
         this._velocity = new Vector(0, 0);
+        this._acceleration = new Vector(0, 0);
     }
 
     /**
      * Run this on every new frame.
      */
     public update(): void {
+        this._velocity.add(this._acceleration);
         this._position.add(this._velocity);
+        this._acceleration.mul(0);
     }
 
     /**
@@ -34,5 +38,9 @@ export default abstract class GameObject {
 
     protected get velocity(): Vector {
         return this._velocity;
+    }
+
+    protected get acceleration(): Vector {
+        return this._acceleration;
     }
 }
