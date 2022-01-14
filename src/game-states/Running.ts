c4d157bc8a0127423ab40f09b38a23ca02db9669
Zefanja Jobse <zefanjaj@gmail.com>
2022-01-14 11:02:54 +0000

    readd back

--- src/game-states/Running.ts
+++ src/game-states/Running.ts
@@ -1,14 +1,16 @@
-import { Pipe, Birb } from "../game-elements/GameObjectExports";
+import * as GameObjects from "../game-elements/GameObjectExports";
 import Vector from "../utils/Vector";
 import GameState from "./GameState";
 
 export default class Running implements GameState {
-    private _pipes: Pipe[];
-    private _player: Birb;
+    private _pipes: GameObjects.Pipe[];
+    private _player: GameObjects.Birb;
+    private _background: GameObjects.Background;
 
     public constructor(ctx: CanvasRenderingContext2D) {
+        this._background = new GameObjects.Background(ctx, new Vector(0, 0))
         this._pipes = [];
-        this._player = new Birb(ctx, new Vector(0, 0));
+        this._player = new GameObjects.Birb(ctx, new Vector(0, 0));
         // this._gameObjects.push();
     }
 
@@ -19,6 +21,7 @@ export default class Running implements GameState {
     public render(): void {
         // Fill the void
         console.log("Render a running game.");
+        this._background.render();
         this._player.render();
         this._pipes.forEach((pipe) => pipe.render());
     };
