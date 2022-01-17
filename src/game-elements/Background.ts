316d0f4bd237a03cf530509637b479088dcd435e
Zefanja Jobse <zefanjaj@gmail.com>
2022-01-17 16:31:18 +0000

    rename of backgroundColors

--- src/game-elements/Background.ts
+++ src/game-elements/Background.ts
@@ -1,49 +1,50 @@
 import Vector from "../utils/Vector";
-import BackgroundColors from "./background_colors/BackgroundColors";
+import Backgrounds from "./background_colors/Backgrounds";
 import GameObject from "./GameObject";
 
 export default class Background extends GameObject {
     public static readonly BACKGROUND_SPEED = 1.25;
-    private _backgroundColors: BackgroundColors[];
+    private _scrollingPictures: Backgrounds[];
 
     public constructor(ctx: CanvasRenderingContext2D, position: Vector) {
         super(ctx, position);
-        this._backgroundColors = [];
+        this._scrollingPictures = [];
         for (let i = 0; i < 2; i++) {
             this.createNewBackground();
         }
     }
 
     public render(): void {
-        this._backgroundColors.forEach(background => {
+        this._scrollingPictures.forEach(background => {
             background.render();
         });
     }
 
     public update(): void {
-        this._backgroundColors.forEach(background => {
+        this._scrollingPictures.forEach(background => {
             background.update();
         });
-        const currentBackground = this._backgroundColors[0]
+        const currentBackground = this._scrollingPictures[0]
         if (currentBackground.onScreen()) {
             this.createNewBackground();
         }
         if (currentBackground.offScreen()) {
-            this._backgroundColors.shift()
+            this._scrollingPictures.shift()
         }
     }
 
     /**
      * Add new backgrounds
      */
-     private createNewBackground(): void {
+    private createNewBackground(): void {
         const backgroundPosition = new Vector(
-            this._backgroundColors.length === 0 ? 0 : this.ctx.canvas.width*2, 0
+            // use beginning position for first background, otherwise add to the back
+            this._scrollingPictures.length === 0 ? 0 : this.ctx.canvas.width * 2, 0
         );
-        const newPipe = new BackgroundColors(
+        const newPipe = new Backgrounds(
             this.ctx,
             backgroundPosition
         );
-        this._backgroundColors.push(newPipe);
+        this._scrollingPictures.push(newPipe);
     }
 }
--- src/game-elements/background_colors/Backgrounds.ts
+++ src/game-elements/background_colors/Backgrounds.ts
@@ -2,19 +2,19 @@ import Vector from "../../utils/Vector";
 import Background from "../Background";
 import GameObject from "../GameObject";
 
-export default class BackgroundColor extends GameObject {
+export default class Backgrounds extends GameObject {
     private static readonly CURRENT_IMG = "./assets/images/backgrounds/1.png"
     private _image = new Image();
     private _sendOnScreen: boolean = false;
 
     public constructor(ctx: CanvasRenderingContext2D, position: Vector) {
         super(ctx, position);
-        this._image.src = BackgroundColor.CURRENT_IMG;
+        this._image.src = Backgrounds.CURRENT_IMG;
         this.velocity.sub(new Vector(Background.BACKGROUND_SPEED, 0))
     }
 
     public render(): void {
-        this.ctx.drawImage(this._image, this.position.x, this.position.y, this.ctx.canvas.width*2, this.ctx.canvas.height);
+        this.ctx.drawImage(this._image, this.position.x, this.position.y, this.ctx.canvas.width * 2, this.ctx.canvas.height);
     }
 
     /**
@@ -34,6 +34,6 @@ export default class BackgroundColor extends GameObject {
      * @returns bool if true
      */
     public offScreen(): boolean {
-        return (this.position.x + this.ctx.canvas.width*2) < 0;
+        return (this.position.x + this.ctx.canvas.width * 2) < 0;
     }
 }
